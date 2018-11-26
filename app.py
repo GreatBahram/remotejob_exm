f970803c30843ef7aadd9fc0fd3f1d07a2a3ecbb
GreatBahram <aghaee.bahram@gmail.com>
2018-11-26 13:11:04 +0330

    add final changes

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import json
 import os
+from hashlib import md5
 
+import requests
 from flask import (Flask, Response, jsonify, request, send_from_directory,
                    url_for)
-
-from utils import random_name, plot_that, async_plot
+from utils import (async_plot, custom_plot, excel_handler, plot_that,
+                   random_name)
 
 app = Flask(__name__)
 
@@ -18,7 +20,7 @@ def index():
     """List of routes for this API."""
     output = {
             'Post dip and strike': 'POST /plot',
-            'Download specific filename': 'GET /download/{filename}',
+#            'Download specific filename': 'GET /download/{filename}',
             }
     
     response = jsonify(output)
@@ -28,7 +30,6 @@ def index():
 @app.route('/plot', methods=['POST'])
 def plot():
     """GET dip and strike from the user and save the output"""
-
     needed = ['dip', 'strike']
 
     if request.content_type == 'application/json':
@@ -42,14 +43,66 @@ def plot():
             strike = int(data.get('strike'))
         except:
             return jsonify({'message':'dip and strike should be integer.'}), 400
-        filename = async_plot(dip, strike)
+        filename = md5('{}{}'.format(dip, strike).encode('utf-8')).hexdigest() + '.xlsx'
+        print(filename)
+        line , triangle, blue_point, orange_point = custom_plot(dip, strike)
+        with excel_handler(filename) as wb:
+            # write basic information
+            work_sheet = wb.active
+            work_sheet['A1'] =  'Dip'
+            work_sheet['B1'] =  'Strike'
+            work_sheet['A2'] =  dip
+            work_sheet['B2'] =  strike
+            work_sheet.title = 'Data'
+
+            # write output of the program
+            work_sheet = wb.create_sheet('Plane points')
+            a, b = line[0].get_data()
+            work_sheet['A1'] = 'A'
+            work_sheet['B1'] = 'B'
+            for index, (a_data, b_data) in enumerate(zip(a, b)):
+                work_sheet.cell(row=index+2, column=1).value = a_data
+                work_sheet.cell(row=index+2, column=2).value = a_data
+
+            work_sheet = wb.create_sheet('Pole points')
+            x, y = triangle[0].get_data()
+            work_sheet['A1'] = 'x'
+            work_sheet['B1'] = 'y'
+            work_sheet['A2'] = x[0]
+            work_sheet['B2'] = y[0]
+
+            work_sheet = wb.create_sheet('Rake points')
+            x, y = blue_point[0].get_data()
+            work_sheet['A1'] = 'x'
+            work_sheet['B1'] = 'y'
+            work_sheet['A2'] = x[0]
+            work_sheet['B2'] = y[0]
+
+            work_sheet = wb.create_sheet('Line points')
+            x, y = orange_point[0].get_data()
+            work_sheet['A1'] = 'x'
+            work_sheet['B1'] = 'y'
+            work_sheet['A2'] = x[0]
+            work_sheet['B2'] = y[0]
+
+        # post data into Kashef.io
+        output = requests.post("http://kcs.kashef.io/upload/", files={"file":open(filename,'rb')})
+        if 'url' in output.json():
+            message = 'Data has been saved.'
+            response = Response(
+                    response=json.dumps({'message': message}),
+                    mimetype="application/json",
+                    status=201)
+        else:
+            message = 'Something bad happended during upload file into kashef'
+            response = Response(
+                    response=json.dumps({'message': message}),
+                    mimetype="application/json",
+                    status=400)
+        # save the pircture and return filename
+        #filename = async_plot(dip, strike)
         #filename = plot_that(dip, strike)
         #filepath = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], filename)
-        response = Response(
-                response=json.dumps({'filename': filename}),
-                mimetype="application/json",
-                status=201
-                )
     else:
         response = Response(
             mimetype="application/json",
@@ -58,15 +111,15 @@ def plot():
             )
     return response
 
-@app.route('/download/', methods=['GET'])
-@app.route('/download/<path:filename>', methods=['GET'])
-def download(filename=None):
-    if filename:
-        if os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], filename)):
-            return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
-        else:
-            return jsonify({'message':"File '{}' does not exist.".format(filename)}), 404
-    return jsonify({'message':'filename cannot left be blank!'}), 400
+#@app.route('/download/', methods=['GET'])
+#@app.route('/download/<path:filename>', methods=['GET'])
+#def download(filename=None):
+#    if filename:
+#        if os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], filename)):
+#            return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+#        else:
+#            return jsonify({'message':"File '{}' does not exist.".format(filename)}), 404
+#    return jsonify({'message':'filename cannot left be blank!'}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import contextlib
 import os
 from hashlib import md5
 from threading import Thread
@@ -5,6 +6,7 @@ from threading import Thread
 import mplstereonet
 from flask import current_app
 from matplotlib import pyplot as plt
+from openpyxl import Workbook
 
 IMAGE_FMT = 'png'
 
@@ -30,9 +32,28 @@ def plot_that(dip, strike, picture_path):
     fig.savefig(picture_path, dpi=200, format=IMAGE_FMT)
     #return filename
 
+def custom_plot(dip, strike):
+    """ Plot the figure and save into app.config['UPLOAD_FOLDER'] directory """
+    plunge, bearing = dip, strike
+    fig = plt.figure()
+    ax = fig.add_subplot(121, projection='stereonet')
+    line = ax.plane(strike, dip, 'g-', linewidth=2)
+    triangle = ax.pole(strike, dip, 'g^', markersize=18)
+    blue_point = ax.rake(strike, dip, -25)
+    orange_point = ax.line(plunge, bearing)
+    return (line , triangle, blue_point, orange_point)
+    
+
 
 def async_plot(dip, strike):
     """ Run plot_that function with another thread in order to make the program faster"""
     picture_path, filename = random_name(dip, strike)
     Thread(target=plot_that, args=(dip, strike, picture_path,)).start()
     return filename
+
+@contextlib.contextmanager
+def excel_handler(filename):
+    wb = Workbook()
+    yield wb 
+    wb.save(filename)
+    wb.close()
